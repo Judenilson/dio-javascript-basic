b1846216f38032aa92c07f9984bc1a89fa6ccf0c
judenilson <judenilson@hotmail.com>
2022-11-24 21:05:59 -0300

    update code for run in local mode

--- basics.js
+++ basics.js
@@ -13,7 +13,8 @@ A saída deverá retornar o valor da soma de todos os múltiplos A até o seu li
 Entrada	Saída
 5       50
 20	
-*/
+
+CÓDIGO ENVIADO:
 var a = parseInt(gets());
 var N = parseInt(gets());
 let result = 0;
@@ -23,6 +24,17 @@ for (let i = 0; i <= N; i++){
   }
 }
 print(result);
+*/
+
+var a = 5
+var N = 20
+let result = 0;
+for (let i = 0; i <= N; i++){
+  if (i % a === 0){
+    result += i;
+  }
+}
+console.log(result);
 
 /*
 Desafio 2
@@ -36,10 +48,9 @@ A saída consiste no retorno da altura máxima que o triângulos de moedas terá
 Entrada	Saída
 7	    3
 Explicação: A altura máxima será de 3, colocando 1, 2 e então 3 moedas. Não é possível usar a moeda que irá sobrar.
-*/
 
+CÓDIGO ENVIADO:
 let N = parseInt(gets()); 
-let n = 1 + 8*N; 
 let qtdLinhas = 0;
 let somaMoedas = 0;
 let i = 1;
@@ -52,6 +63,21 @@ while(i < N){
   i++;
 }
 print(qtdLinhas);
+*/
+
+let valorMoeda = 7
+let qtdLinhas = 0;
+let somaMoedas = 0;
+let i = 1;
+while(i < valorMoeda){
+  somaMoedas += i
+  if (somaMoedas > valorMoeda){
+    break;
+  }
+  qtdLinhas += 1;
+  i++;
+}
+console.log(qtdLinhas);
 
 /*Desafio 3
 O programa deve ler um valor inteiro D indefinidas vezes, ele só irá parar quando o valor de D for igual a 0. Para cada D lido, imprima a soma dos 5 pares consecutivos a partir de D, inclusive ele mesmo , se for par. Se o valor de entrada for 4, por exemplo, a saída deve ser 40, que é o resultado da operação: 4+6+8+10+12, enquanto que se o valor de entrada for 11, por exempo, a saída deve ser 80, que é a soma de 12+14+16+18+20.
@@ -65,7 +91,8 @@ Exemplo de Entrada  Exemplo de Saída
 4                   40
 11                  80
 0
-*/
+
+CÓDIGO ENVIADO:
 let x = parseInt(gets());
 while (x !== 0)
 {
@@ -81,6 +108,22 @@ while (x !== 0)
   print(y);
   x = parseInt(gets());
 }
+*/
+let x = 4
+while (x !== 0)
+{
+  let y = 0;
+  if  (x % 2 !== 0){ 
+    x = x + 1;
+  }
+  y = x;
+  for(let i = 0; i < 4; i++){
+    x += 2;
+    y += x;
+  }
+  console.log(y);
+  x = 0;
+}
 
 /*
 Desafio 4
@@ -93,12 +136,19 @@ Saída
 A saída deverá retornar “A area da circunferencia é: “,  e em seguida o valor do perimetro do triângulo, conforme exemplo abaixo.
 Entrada	Saída
 6	    A area da circunferencia eh: 37.70
-*/
+
+CÓDIGO ENVIADO:
 let pi = 3.14159265358979323846;
 let lado = gets();
 let area = 0;
 area = ((lado * lado)/3) * pi;
 print("A area da circunferencia eh: " + area.tofixed(2));
+*/
+let pi = 3.14159265358979323846;
+let lado = 6;
+let area = 0;
+area = ((lado * lado)/3) * pi;
+console.log("A area da circunferencia eh: " + area.toFixed(2));
 
 /*
 Desafio 5
@@ -115,8 +165,8 @@ Explicação:
 Produto de dígitos = 2 * 3 * 4 = 24
 Soma dos dígitos = 2 + 3 + 4 = 9
 Resultado = 24 - 9 = 15
-*/
 
+CÓDIGO ENVIADO:
 let lines = gets(); 
 let soma = 0;
 let produto = 1;
@@ -127,3 +177,15 @@ for (let i = 0; i < lines.length; i++) {
     produto *= digito;
 }
 print(produto - soma);
+*/
+
+let lines = "234"; 
+let soma = 0;
+let produto = 1;
+
+for (let i = 0; i < lines.length; i++) {
+    let digito = parseInt(lines[i], 10); 
+    soma += digito;
+    produto *= digito;
+}
+console.log(produto - soma);
